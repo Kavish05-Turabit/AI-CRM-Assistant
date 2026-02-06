524b30c5330b34072e4307f1d186021e6937525f
Kavish05-Turabit <kavish.turabit@gmail.com>
2026-02-06 16:36:30 +0530

    chore: Added a file which was missed.

--- views/chat_view.py
+++ views/chat_view.py
@@ -2,7 +2,7 @@ import streamlit as st
 from ai_core.agent import GeminiAssistant
 
 st.title("AI Assistant")
-st.set_page_config(layout="centered",page_title="AI CRM Assistant")
+st.set_page_config(layout="wide",page_title="AI CRM Assistant")
 
 
 if "messages" not in st.session_state:
@@ -13,7 +13,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if "agent" not in st.session_state:
-    st.session_state["agent"] = GeminiAssistant(api_key="AIzaSyDdHKYfOnQD9s9O-iMRPfqgNAfUFcwJ9bo")
+    st.session_state["agent"] = GeminiAssistant(model="groq")
 
 llm = st.session_state.agent
 
@@ -23,10 +23,10 @@ if prompt := st.chat_input("Ask something..."):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    ai_response = llm.send_message(prompt)
-
-    st.session_state.messages.append({"role": "ai", "content": ai_response})
     with st.chat_message("ai"):
-        st.markdown(ai_response)
+        with st.spinner("Loading..."):
+            ai_response = llm.send_message(prompt)
+            st.markdown(ai_response)
+            st.session_state.messages.append({"role": "ai", "content": ai_response})
 
 
